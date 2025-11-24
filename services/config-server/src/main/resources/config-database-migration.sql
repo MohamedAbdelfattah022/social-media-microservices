0853e69f5e269ca26a3ef98e8c8912db52271f59
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2025-11-24 15:16:34 +0200

    Added Eureka discovery service

--- services/config-server/src/main/resources/config-database-migration.sql
+++ services/config-server/src/main/resources/config-database-migration.sql
@@ -2,3 +2,15 @@ INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
 VALUES ('application', 'default', 'master', 'logging.pattern.console',
         '%green(%d{HH:mm:ss.SSS}) %blue(%-5level) %red([%thread]) %yellow(%logger{15}) - %msg%n'),
        ('application', 'default', 'master', 'management.endpoints.web.exposure.include', '*');
+
+
+INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+VALUES ('application', 'default', 'master', 'eureka.instance.prefer-ip-address', 'true'),
+       ('application', 'default', 'master', 'eureka.client.fetch-registry', 'true'),
+       ('application', 'default', 'master', 'eureka.client.service-url.defaultZone', 'http://localhost:8761/eureka/');
+
+
+INSERT INTO PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+VALUES ('discovery-service', 'default', 'master', 'HOST_NAME', 'localhost'),
+       ('discovery-service', 'default', 'master', 'PORT', '8761'),
+       ('discovery-service', 'default', 'master', 'ZONE', 'http://${eureka.instance.hostname}:${server.port}/eureka/');
