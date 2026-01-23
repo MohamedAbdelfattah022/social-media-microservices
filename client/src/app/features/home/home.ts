65824adb618e35debfb70b02506c4e9e729218df
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2026-01-23 21:04:24 +0200

    refactor(features): update home, login and signup integration

--- client/src/app/features/home/home.ts
+++ client/src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Feed } from '../feed/feed';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [Feed, Sidebar],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
